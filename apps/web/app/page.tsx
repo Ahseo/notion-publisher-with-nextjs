ad76f9018bffa3197e1dcb8930c52fd16a03a740
ahseoKoo <53210177+Ahseo@users.noreply.github.com>
2024-10-11 12:30:59 +0900

    feat: add notion page id

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -10,7 +10,7 @@ import { getChildrenBlocks, getNotionBlocks } from "../lib/utils/notion";
   notion의 pageId를 사용합니다.
 */
 
-const notionPageId = "";
+const notionPageId = "370944a1-c6af-4727-9419-011b0a3a5ce2";
 
 export default async function Home() {
   try {
